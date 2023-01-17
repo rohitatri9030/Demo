9539cc265713005a3a27a4e3f80ab1bfa9dd40e4
rohitatri9030 <rohitatri9030@gmail.com>
2023-01-17 16:03:45 +0530

    first commit

--- one.dart
+++ one.dart
@@ -1,1 +1,10 @@
 // 1
+//"panner butter masala | 300.5"
+//print "item_name is of cost item_cost"
+
+void printAns() {
+  String myInitistring = "Paneer Butter masala | 300.5";
+}
+
+// foodname1 is of foodcost1, and discounted amount is doscountedamount1
+//foodname 2 is of foodcast2, and discounted amout is discountedamount2
